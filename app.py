d30c40b1784c8d96cb1ae91c93a36d4feefa4524
stefania5964 <tefila123@hotmail.com>
2023-10-17 19:26:45 -0500

    docker

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
+@app.route('/saludo')
+def saludo():
+    return '¡Hola, mundo desde Colombia'
 if __name__ == '__main__':
     app.run(debug = True, port = 4000)
